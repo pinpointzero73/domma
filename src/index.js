b1148bce55d788ee22c4f15ba61edda7ab8d664f
Darryl Waterhouse <darryl@dcbw-it.co.uk>
2025-12-03 21:15:15 +0000

    For: [main]
    Additions:
    - Added new dependencies such as `from`, `get-value`, and `glob-parent` to `package.json`.
    - Updated the dependencies tree in `package-lock.json`.
    
    Amendments:
    - None
    
    Removals:
    - None

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import { dom } from './dom.js';
-import { http } from './http.js';
-import { utils } from './utils.js';
-import { configEngine } from './config.js';
+import {dom} from './dom.js';
+import {http} from './http.js';
+import {utils} from './utils.js';
+import {configEngine} from './config.js';
+import {models} from './models.js';
+import {elements} from './elements.js';
+import {dates} from './dates.js';
+import {tables} from './tables.js';
 
 const Domma = (selector) => dom(selector);
 
@@ -9,17 +13,29 @@ const Domma = (selector) => dom(selector);
 Domma.http = http;
 Domma.utils = utils;
 Domma.setup = (config) => configEngine.process(config);
+Domma.models = models;
+Domma.elements = elements;
+Domma.dates = dates;
+Domma.tables = tables;
 
 // Short aliases
 const $ = Domma;
 const _ = utils;
+const M = models;
+const D = dates;
+
+// Attach aliases to Domma
+Domma.M = M;
+Domma.D = D;
 
 // Expose globally if needed
 if (typeof window !== 'undefined') {
     window.Domma = Domma;
     window.$ = $;
     window._ = _;
+    window.M = M;
+    window.D = D;
 }
 
 export default Domma;
-export { Domma, $ , _ };
+export {Domma, $, _, M, D};
